41b15fd837649f4792a611928b2460cd67ad6d55
Alex Golyashov <alex.golyashov@rangle.io>
2016-11-06 18:04:40 -0500

    remove compiled files

--- src/app/service/classes/playoffsenum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var PlayoffsEnum;
-(function (PlayoffsEnum) {
-    PlayoffsEnum[PlayoffsEnum["none"] = 0] = "none";
-    PlayoffsEnum[PlayoffsEnum["bestvsworst"] = 1] = "bestvsworst";
-})(PlayoffsEnum || (PlayoffsEnum = {}));
-exports.PlayoffsEnum = PlayoffsEnum;
-//# sourceMappingURL=playoffsenum.js.map
--- src/app/service/classes/scheduleenum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var ScheduleEnum;
-(function (ScheduleEnum) {
-    ScheduleEnum[ScheduleEnum["roundrobin"] = 0] = "roundrobin";
-    ScheduleEnum[ScheduleEnum["random"] = 1] = "random";
-    ScheduleEnum[ScheduleEnum["ladder"] = 2] = "ladder";
-})(ScheduleEnum || (ScheduleEnum = {}));
-exports.ScheduleEnum = ScheduleEnum;
-//# sourceMappingURL=scheduleenum.js.map
